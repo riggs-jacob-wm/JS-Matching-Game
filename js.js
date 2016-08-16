39712625bcf5b84156e73ffe7d7e5d274615e2c5
Jacob <jacob.riggs@west-mec.org>
2016-08-16 15:16:49 -0700

    Finished the game.

--- js.js
+++ js.js
@@ -5,18 +5,57 @@ var numberOfFaces = 5;
 
 var theLeftSide = document.getElementById("leftside");
 
+var theRightSide = document.getElementById("rightside");
+
+var theBody = document.getElementsByTagName("body")[0];
+
+// The variables that will be used.
+
 function generateFaces() {
 
     for(var i = 0; i < numberOfFaces; i++) {
         var image = document.createElement("img");
 
-        image.src = "http://cliparting.com/wp-content/uploads/2016/05/Smiley-face-clip-art-images-free-clipart-images.jpeg";
-
+        image.src = "http://vignette2.wikia.nocookie.net/pkmnshuffle/images/f/f6/Icon_Eevee.png/revision/latest?cb=20150225004826";
         image.style.top = (Math.random() * 400) + "px";
         image.style.left = (Math.random() * 400) + "px";
 
+
         theLeftSide.appendChild(image);
+
+// Makes the left side images
     }
+
+    var leftSideImages = theLeftSide.cloneNode(true);
+
+// Clones the left side
+
+    leftSideImages.removeChild(leftSideImages.childNodes[leftSideImages.childNodes.length-1]);
+
+// Removes one of the pictures from th right
+
+    theRightSide.appendChild(leftSideImages);
+
+    // Makes the right side images
+
+    theLeftSide.lastChild.onclick=
+        function nextLevel(event){
+            event.stopPropagation();
+            numberOfFaces += 5;
+            generateFaces();
+        };
+
+// Makes the game continues if right
+
+    theBody.onclick = function gameOver() {
+        alert("Game Over!");
+
+        theBody.onclick = null;
+        theLeftSide.lastChild.onclick = null;
+    };
+
+// Ends the game if wrong
+
 }
 
 
